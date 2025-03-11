ef9306b8d07c956bbb9c3c6a20737742b3d0c84a
John Maicon <john.pereira@localiza.com>
2025-03-10 22:17:26 -0300

    start project

---
+++ src/main/java/com/vitrine_freelancers_server/VitrineFreelancersServerApplication.java
@@ -0,0 +1,13 @@
+package com.vitrine_freelancers_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrineFreelancersServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitrineFreelancersServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitrine_freelancers_server/VitrineFreelancersServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitrine_freelancers_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitrineFreelancersServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
